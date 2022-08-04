709fe86b340e86d49f2932eba52d98f352189bf1
jhyunwoo <jhyunwoo0228@gmail.com>
2022-08-04 23:01:32 +0900

    start note us app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Beatus Note Us</Text>
       <StatusBar style="auto" />
     </View>
   );
